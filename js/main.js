b52b07ad9fe71786de1897430367b7a62c726aaf
mhassaannaveed <muhammadhassaannaveed904@gmail.com>
2023-08-20 18:44:39 +0500

    Input Field changes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btnDiv =  document.getElementById('btnsDiv');
+// btnDiv.style.display = 'none';
+// let open=()=>{
+//     console.log('helo wroe');
+// }
+
+let close=()=>{
+    console.log('fjdf dfor')
+}
