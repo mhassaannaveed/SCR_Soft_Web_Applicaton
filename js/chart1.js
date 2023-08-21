f8abfc082a697c1ea5574d9c017561f7c69a2ffa
afaq <afaqak124@gmail.com>
2023-08-21 18:02:51 +0500

    feat:implement dashboard2 charts

--- js/chart1.js
+++ js/chart1.js
@@ -1,20 +1,50 @@
-const ctx = document.getElementById('lineChart1');
+const ctx = document.getElementById("lineChart1");
 
 const lineChart1 = new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: {
-    labels: ['July', 'July', 'July', 'July', 'July', 'July'],
-    datasets: [{
-      label: 'current week',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
+    labels: ["July 17", "July 19", "July 21", "July 23", "July 25", "July 27"],
+    datasets: [
+      {
+        label: "Current Week",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 3,
+        borderColor: "#2E4EA5",
+      },
+      {
+        label: "Previous Week",
+        data: [5, 8, 14, 7, 9, 11],
+        borderWidth: 3,
+        borderColor: "#90BE6D",
+      },
+    ],
   },
   options: {
     scales: {
+      x: {
+        beginAtZero: true,
+      },
       y: {
-        beginAtZero: true
-      }
-    }
-  }
+        beginAtZero: true,
+        ticks: {
+          stepSize: 8,
+          display: false,
+        },
+        grid: {
+          display: true,
+          drawBorder: false,
+        },
+      },
+    },
+
+    plugins: {
+      legend: {
+        position: "top",
+        align: "start",
+        labels: {
+          usePointStyle: true,
+        },
+      },
+    },
+  },
 });
--- js/chart2.js
+++ js/chart2.js
@@ -1,22 +1,37 @@
-const cto = document.getElementById('doughnutChart');
+const cto = document.getElementById("doughnutChart");
 const data = {
-    labels: [
-        'Sales',
-        'Completed',
-        'Pending'
-    ],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [70, 25, 5],
-        backgroundColor: [
-            'rgba(46, 78, 165, 1)',
-            'rgba(249, 199, 79, 1)',
-            'rgba(144, 190, 109, 1)'
-        ],
-        hoverOffset: 4
-    }]
+  labels: ["Sales", "Completed", "Pending"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [70, 25, 5],
+      backgroundColor: [
+        "rgba(46, 78, 165, 1)",
+        "rgba(249, 199, 79, 1)",
+        "rgba(144, 190, 109, 1)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
 };
+
+const options = {
+  maintainAspectRatio: false,
+  cutout: 60,
+  borderWidth: 0,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    labels: {
+      render: "percentage",
+      fontColor: "#fff",
+    },
+  },
+};
+
 const doughnut = new Chart(cto, {
-    type: 'doughnut',
-    data: data,
-})
+  type: "doughnut",
+  data: data,
+  options: options, // Include the options configuration
+});
