1876e7517f296d3c136f3b9c7107258573370c4b
mhassaannaveed <muhammadhassaannaveed904@gmail.com>
2023-08-21 19:04:40 +0500

    Changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 let start = () => {
     const middleDiv = document.querySelector('.middle-div');
-    middleDiv.classList.add('active'); // Add the active class to show the div
+    middleDiv.classList.add('active');
 }
 
 let end = () => {
     const middleDiv = document.querySelector('.middle-div');
-    middleDiv.classList.remove('active'); // Remove the active class to hide the div
+    middleDiv.classList.remove('active'); 
 }
