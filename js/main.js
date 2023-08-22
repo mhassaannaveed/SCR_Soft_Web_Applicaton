8eb57a1784de8ad6d00e4bf50999b49ac2426207
mhassaannaveed <muhammadhassaannaveed904@gmail.com>
2023-08-23 03:37:19 +0500

    Setting up the responsive navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
+const overlay = document.getElementById("overlay");
+const togglenavbar = document.getElementById("togglenavbar");
+const middleDiv = document.querySelector(".middle-div");
+const toggleButton = document.querySelector(".navbar-toggler");
 
 let start = () => {
-    const middleDiv = document.querySelector('.middle-div');
-    middleDiv.classList.add('active');
-}
+  middleDiv.classList.add("active");
+  togglenavbar.style.left = "-300px";
+  overlay.style.display = "none";
+  toggleButton.style.display = "none";
+};
 
 let end = () => {
-    const middleDiv = document.querySelector('.middle-div');
-    middleDiv.classList.remove('active'); 
-}
+  middleDiv.classList.remove("active");
+  togglenavbar.style.left = "0";
+  overlay.style.display = "block";
+  toggleButton.style.display = "flex";
+};
+
+console.log(toggleButton, "tog");
+
+let isNavbarOpen = false;
+
+toggleButton.addEventListener("click", () => {
+  if (isNavbarOpen) {
+    togglenavbar.style.left = "-300px";
+    overlay.style.display = "none";
+    toggleButton.classList.remove("open");
+  } else {
+    togglenavbar.style.left = "0";
+    overlay.style.display = "block";
+    toggleButton.classList.add("open");
+  }
+
+  isNavbarOpen = !isNavbarOpen;
+});
+
+overlay.addEventListener("click", () => {
+  togglenavbar.style.left = "-300px";
+  overlay.style.display = "none";
+});
+
+$(document).ready(function () {
+  $("#openMiddleDivBtn").click(function () {
+    $(".middle-div").toggleClass("active");
+  });
+});
